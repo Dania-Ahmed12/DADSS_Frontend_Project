abdcfae70d9c99fc5144a11ea6e2fa1080336c16
Dania-Ahmed12 <daniaahmed704@gmail.com>
2024-02-02 14:34:31 +0500

    merchant trip

--- pages/merchantVesselDetails/index.jsx
+++ pages/merchantVesselDetails/index.jsx
@@ -66,7 +66,7 @@ const index = () => {
         />
       </div>
       <div>
-        <AntdTable columns={columns} data={data} loading={isLoading}  />
+        <AntdTable columns={columns} data={data} loading={isLoading} scrollConfig={{x:true}}  />
       </div>
     </>
   );
